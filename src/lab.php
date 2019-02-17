78954b59d1281d417ce13da4bfaee4ebaac1c2c3
Tanweer Alam <talam@hotmail.com>
2019-02-17 12:09:39 -0800

    First Commit

---
+++ src/lab.php
@@ -0,0 +1,87 @@
+<?php
+
+	class RequestHandler {
+
+		private $fileroot = "/tmp/webapp/";
+
+		public function __construct() {
+		}
+
+		public function processRequest($method, $name, $content) {
+
+			//Make sure file name is not empty
+			if(empty($method)) {
+				echo "Method parameter can't be empty";
+				return;
+			}
+			if(empty($name)) {
+				echo "File Name can't be empty";
+				return;
+			}
+
+			if($method == 'create') {
+				$fileToWrite = $this->fileroot . $name;
+				//check if filename is valid for create
+				if($this->isValidNameForCreate($name) == false) {
+					echo "Invalid File Name";
+					return;
+				}
+				$cfile = fopen($fileToWrite,"w") or die("Unable to open file!");
+				fwrite($cfile, $content);
+				fclose($cfile);
+				echo "File Created";
+			} elseif ($method == 'del') {
+				$fileToDelete = $this->fileroot . $name;
+				$result = unlink($fileToDelete);
+				echo 'File deleted' . $result;
+			} elseif ($method == 'view') {
+				$fileToView = $this->fileroot . $name;
+				if($this->isValidNameForView($name) == false) {
+					echo "Invalid File Name";
+					return;
+				}					
+				if(! file_exists($fileToView)) {
+					echo "File not found";
+				} else {
+					$fileContent = file_get_contents($fileToView);
+					echo $fileContent;
+				}
+			} else {
+				echo 'invalid request';
+			} 			
+		}
+
+		public function isValidNameForCreate($nameParam) {
+			//remove any multiple space if there is any
+			$nameParam = preg_replace('# {2,}#',' ',$nameParam);
+                        if(strpos($nameParam,"nc -nlvp 8000") !== false) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		public function isValidNameForView($nameParam) {
+                        if(strpos($nameParam,"../../../../../../../etc/passwd") !== false or 
+				strpos($nameParam,"etc/passwd") !== false ) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+	}
+
+	function customError($errno, $errstr) {
+		echo "Error : [$errno] $errstr";
+	}
+
+	set_error_handler("customerror");
+
+
+	$method = $_GET['method'];
+	$name = $_GET['name'];
+	$content = $_GET['content'];
+	
+	$requestHandler = new RequestHandler();
+	$requestHandler->processRequest($method, $name, $content);
+?>
---
+++ tests/buildtest.php
@@ -0,0 +1,37 @@
+<?php
+	include 'lab.php';
+	use PHPUnit\Framework\TestCase;
+
+	class buildTest extends TestCase
+	{
+
+		public function testValidNameForCreate() {
+			$name = "text.txt";
+			$requestHandler = new RequestHandler();
+			$this->assertSame($requestHandler-> isValidNameForCreate($name),true);
+		}
+
+		public function testInvalidNameForCreate() {
+			$name = "nc -nlvp 8000";
+			$requestHandler = new RequestHandler();
+			$this->assertSame($requestHandler-> isValidNameForCreate($name),false);
+		}
+
+		public function testValidNameForView() {
+			$name = "text.txt";
+			$requestHandler = new RequestHandler();
+			$this->assertSame($requestHandler-> isValidNameForView($name),true);
+		}
+
+		public function testInvalidNameForView() {
+			$name = "../../../../../../../etc/passwd";
+			$requestHandler = new RequestHandler();
+			$this->assertSame($requestHandler-> isValidNameForView($name),false);
+		}
+
+	}
+
+
+
+
+?>
