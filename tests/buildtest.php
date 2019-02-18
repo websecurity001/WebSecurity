e2407f5a619dacb6bfd9cd21940301f5ddc59188
Tanweer Alam <talam@hotmail.com>
2019-02-17 18:32:02 -0800

    Adding @small tag

--- tests/buildtest.php
+++ tests/buildtest.php
@@ -4,25 +4,36 @@
 
 	class buildTest extends TestCase
 	{
-
+		/**
+		 * @small
+		 */
 		public function testValidNameForCreate() {
 			$name = "text.txt";
 			$requestHandler = new RequestHandler();
 			$this->assertSame($requestHandler-> isValidNameForCreate($name),true);
 		}
 
+		/**
+		 * @small
+		 */
 		public function testInvalidNameForCreate() {
 			$name = "nc -nlvp 8000";
 			$requestHandler = new RequestHandler();
 			$this->assertSame($requestHandler-> isValidNameForCreate($name),false);
 		}
 
+		/**
+		 * @small
+		 */
 		public function testValidNameForView() {
 			$name = "text.txt";
 			$requestHandler = new RequestHandler();
 			$this->assertSame($requestHandler-> isValidNameForView($name),true);
 		}
 
+		/**
+		 * @small
+		 */
 		public function testInvalidNameForView() {
 			$name = "../../../../../../../etc/passwd";
 			$requestHandler = new RequestHandler();
